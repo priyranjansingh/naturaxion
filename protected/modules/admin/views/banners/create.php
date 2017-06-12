e2e1c7a7097f8e3f7ee2a1afb3f691246ab602a9
Neeraj Kumar <neeraj24a@gmail.com>
2017-06-13 02:24:41 +0530

    Bug Fixes

--- protected/modules/admin/views/banners/create.php
+++ protected/modules/admin/views/banners/create.php
@@ -1,7 +1,7 @@
 <section class="content-header">
     <h1>
         Add
-        <small>Banner</small>
+        <small>Banners</small>
     </h1>
     <ol class="breadcrumb">
         <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
@@ -14,28 +14,10 @@
         <div class="col-xs-12">
             <div class="box box-info">
                 <div class="box-header with-border">
-                    <h3 class="box-title">Banner</h3>
+                    <h3 class="box-title">Banners</h3>
                 </div>
                 <?php $this->renderPartial('_form', array('model' => $model)); ?>
             </div>
         </div>
     </div>
 </section>
-<?php
-/* @var $this BannersController */
-/* @var $model Banners */
-
-$this->breadcrumbs=array(
-	'Banners'=>array('index'),
-	'Create',
-);
-
-$this->menu=array(
-	array('label'=>'List Banners', 'url'=>array('index')),
-	array('label'=>'Manage Banners', 'url'=>array('admin')),
-);
-?>
-
-<h1>Create Banners</h1>
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
